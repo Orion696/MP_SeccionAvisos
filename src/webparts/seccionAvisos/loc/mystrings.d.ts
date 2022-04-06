3ef97323b31caf02b8a3992b8d7dd5b4231bdec2
Fernando Rodríguez <frodriguez@integrupo.com>
2022-04-06 11:04:15 -0500

    Creación de componente Webpart

---
+++ src/webparts/seccionAvisos/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface ISeccionAvisosWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'SeccionAvisosWebPartStrings' {
+  const strings: ISeccionAvisosWebPartStrings;
+  export = strings;
+}
